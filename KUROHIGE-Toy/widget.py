9e7b8d4d4411692f414719d17c479a281eeb4dfe
YYYY <ysaq3424@gmail.com>
2025-02-06 12:30:01 +0900

    update : バックエンドの開発
    エラー吐いている

--- KUROHIGE-Toy/widget.py
+++ KUROHIGE-Toy/widget.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from PIL import Image
+import window_state_monitor as wsm
 
 FONT_TYPE = ('Arial', 12)
 
@@ -7,13 +8,15 @@ class ResetFrameWidget(ctk.CTkFrame):
     # リセットボタン等を配置
     def __init__(self, master=None):
         super().__init__(master)
+        self.window_state_monitor = wsm.WindowStateMonitor()
         self.create_widgets()
 
     def create_widgets(self):
         self.title_label = ctk.CTkLabel(self, text='KUROHIGE Toy', font=FONT_TYPE)
         self.title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
         
-        self.rest_button = ctk.CTkButton(self, text='Reset', font=FONT_TYPE)
+        self.rest_button = ctk.CTkButton(self, text='Reset', font=FONT_TYPE,
+                                         command=self.window_state_monitor.reset_triger())
         self.rest_button.grid(row=1, column=0, padx=10, pady=10)
 
 class MainFrameWidget(ctk.CTkFrame):
@@ -21,23 +24,27 @@ class MainFrameWidget(ctk.CTkFrame):
     # ここに画像やボタンを配置する
     def __init__(self, master=None):
         super().__init__(master)
-        self.create_button()
+        self.create_widgets()
 
-    def create_button(self):        
+    def create_widgets(self):
+        self.main_label = ctk.CTkLabel(self, text='Main Frame', font=FONT_TYPE)
+        self.main_label.grid(row=0, column=0, padx=10, pady=10)
+        
         self.buttons = {}
-        # 左右に縦に10個ボタンを配置
-        for i in range(3):
-            if i != 1:
-                # i は調節が必要
-                for j in range(10):
-                    button = ctk.CTkButton(self, text=f'Button {j}', command=lambda row=i, col=j: self.on_button_click(f"{row}-{col}"), font=FONT_TYPE)
-                    button.grid(row=j, column=i, padx=20, pady=5)
-                    self.buttons[j] = button
+        for i in range(2):
+            for j in range(10):
+                button = ctk.CTkButton(self, text=f'Button {j}', command=lambda row=i, col=j: self.on_button_click((row, col)), font=FONT_TYPE)
+                button.grid(row=j+1, column=i, padx=5, pady=5)
+                self.buttons[(i, j)] = button
 
-        # configureで更新
-        self.image = ctk.CTkImage(light_image=Image.open(r'KUROHIGE-Toy\images\safe.png'), size=(500, 500))
+        self.image = ctk.CTkImage(light_image=Image.open(r'KUROHIGE-Toy\images\safe.png'))
         self.image_label = ctk.CTkLabel(self, image=self.image, text="")
-        self.image_label.grid(row=0, column=1, padx=20, pady=20, rowspan=10)
+        self.image_label.grid(row=1, column=0, padx=10, pady=10)
+
+        self.window_state_monitor = wsm.WindowStateMonitor(self.buttons, self.image_label)
+        self.window_state_monitor.reset_triger()
 
     def on_button_click(self, button_id):
-        print(f'Button {button_id} clicked')
+        self.window_state_monitor.button_state(button_id)
+
+    
--- KUROHIGE-Toy/window_state_monitor.py
+++ KUROHIGE-Toy/window_state_monitor.py
@@ -1,4 +1,50 @@
-# 画像を変えたりはずれボタンの位置を変えるclass
+import random
+from PIL import Image
+import customtkinter as ctk
 
 class WindowStateMonitor:
-    pass
+    def __init__(self, buttons, image_label):
+        self.matrix = [[False for _ in range(2)] for _ in range(10)]
+        self.buttons = buttons
+        self.image_label = image_label
+
+    def button_state(self, button_id):
+        """
+        ボタンのコールバック
+        はずれボタンが押されたら画像を変える
+        真偽値はreset_trigerの値
+        """
+        row, col = button_id
+        if self.matrix[row][col]:
+            self.change_image()
+        else:
+            self.buttons[button_id].configure(state="disabled")
+
+    def change_image(self):
+        new_image = ctk.CTkImage(light_image=Image.open(r'KUROHIGE-Toy\images\out.png'))
+        self.image_label.configure(image=new_image)
+        self.image_label.image = new_image  # 参照を保持するため
+
+    def reset_triger(self):
+        """
+        buttonは座標で管理する必要がある
+        暫定返り値
+        button_id: int - (row,col)
+        True: はずれ False: セーフ
+
+        追加機能予定
+        - 画像の変更
+        - 無効かされていたボタンの有効化
+        """
+        # 2列10行の行列を作成
+        self.matrix = [[False for _ in range(2)] for _ in range(10)]
+        
+        # 3つのTrueをランダムに配置
+        true_positions = random.sample(range(20), 3)
+        for pos in true_positions:
+            row, col = divmod(pos, 2)
+            self.matrix[row][col] = True
+        
+        # ボタンを有効化
+        for button in self.buttons.values():
+            button.configure(state="normal")
